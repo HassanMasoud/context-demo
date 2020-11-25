1be3a63546a4454d34b3ae985ae01a5fccf25077
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-11-25 08:30:17 +0300

    Add Theme context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import PageContent from "./PageContent";
 import Navbar from "./Navbar";
 import Form from "./Form";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Navbar />
-        <Form />
-      </>
+      <ThemeProvider>
+        <PageContent>
+          <Navbar />
+          <Form />
+        </PageContent>
+      </ThemeProvider>
     );
   }
 }
